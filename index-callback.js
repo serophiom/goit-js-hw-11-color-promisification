959982c83ea83b14a7ddace13b812d4125c4e596
serophiom <serophiom@gmail.com>
2021-05-23 21:58:53 +0300

    добавляет 3 задачу

--- index-callback.js
+++ index-callback.js
@@ -5,14 +5,6 @@ const users = [
   { name: 'Lux', active: false },
 ];
 
-// const toggleUserState = (allUsers, userName, callback) => {
-//   const updatedUsers = allUsers.map(user =>
-//     user.name === userName ? { ...user, active: !user.active } : user,
-//   );
-
-//   callback(updatedUsers);
-// };
-
 const toggleUserState = (allUsers, userName) => {
 
   return new Promise((resolve, reject) => {
